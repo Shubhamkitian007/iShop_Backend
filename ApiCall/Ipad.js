ff364e63d5bee1921fa57f93aed251b42bd0b141
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-08-10 16:17:25 +0530

    Add Catogary

--- ApiCall/Ipad.js
+++ ApiCall/Ipad.js
@@ -3,6 +3,7 @@ const get = (request, response) =>{
         {
             id: 17,
             name: "Ipad Air 5th Gen",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-air-select-wifi-starlight-202203?wid=1765&hei=2000&fmt=jpeg&qlt=95&.v=1645895139236",
             price: "₹54900",
@@ -10,6 +11,7 @@ const get = (request, response) =>{
         {
             id: 18,
             name: "Ipad Air A14",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/71kFzFbiINL._SX522_.jpg",
             price: "65500",
@@ -17,6 +19,7 @@ const get = (request, response) =>{
         {
             id: 19,
             name: "Ipad Air 2",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://drop.ndtv.com/TECH/product_database/images/10172014123206AM_635_apple_ipad_air_2_wifi_apple.jpeg",
             price: "₹49900",
@@ -24,6 +27,7 @@ const get = (request, response) =>{
         {
             id: 20,
             name: "Ipad Air 3rd Gen",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/61z85f1XsgL._SL1500_.jpg",
             price: "₹58900",
@@ -31,6 +35,7 @@ const get = (request, response) =>{
         {
             id: 21,
             name: "Ipad 11 Pro",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://www.aptronixindia.com/media/catalog/product/1/1/11-inch128gbipadprowi-ficelluar-spacegrey.png",
             price: "₹60000",
@@ -38,6 +43,7 @@ const get = (request, response) =>{
         {
             id: 22,
             name: "Ipad Pro 10",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/71JWgeHx45L._AC_SY450_.jpg",
             price: "₹49900",
@@ -45,6 +51,7 @@ const get = (request, response) =>{
         {
             id: 23,
             name: "Ipad Pro M1",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-pro-11-select-202104_GEO_IN_FMT_WHH?wid=2000&hei=2000&fmt=png-alpha&.v=1617900312000",
             price: "₹75000",
@@ -52,6 +59,7 @@ const get = (request, response) =>{
         {
             id: 24,
             name: "Ipad Pro 10.5 Inch.",
+            catagory: "Ipad",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://cdn.alloallo.media/catalog/product/apple/ipad/ipad-pro-10-5-in/ipad-pro-10-5-in-space-gray.jpg",
             price: "₹60000",
--- ApiCall/Mobile.js
+++ ApiCall/Mobile.js
@@ -3,6 +3,7 @@ const get = (request , response) =>{
         {
             id: 1,
             name: "OnePlus 9",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
             price: "₹22000",
@@ -10,6 +11,7 @@ const get = (request , response) =>{
         {
             id: 2,
             name: "OnePlus 10 Pro",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/61mIUCd-37L._SL1500_.jpg",
             price: "₹19500",
@@ -17,6 +19,7 @@ const get = (request , response) =>{
         {
             id: 3,
             name: "OnePlus 10 R",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/716uVx3Wr5L._AC_SS450_.jpg",
             price: "₹35000",
@@ -24,6 +27,7 @@ const get = (request , response) =>{
         {
             id: 4,
             name: "OnePlus Nord 2T",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/617MPEZB5mL._SL1500_.jpg",
             price: "₹16500",
@@ -31,6 +35,7 @@ const get = (request , response) =>{
         {
             id: 5,
             name: "OnePlus Nord CE 2",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/619n6kxuGsL._SL1500_.jpg",
             price: "₹23500",
@@ -38,6 +43,7 @@ const get = (request , response) =>{
         {
             id: 6,
             name: "Redmi Note 9 Pro",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://i01.appmifile.com/webfile/globalimg/in/cms/A0F36EF9-A32C-FF2F-E2D5-1FE5124589C2.jpg",
             price: "₹12500",
@@ -45,6 +51,7 @@ const get = (request , response) =>{
         {
             id: 7,
             name: "Redmi Note 10 Lite",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://i02.appmifile.com/644_operator_in/30/09/2021/71bceaa70d1b789429e32134dc332adb.jpg",
             price: "₹14500",
@@ -52,6 +59,7 @@ const get = (request , response) =>{
         {
             id: 8,
             name: "Redmi Note 10 Prime",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/817clKAKcqL._SL1500_.jpg",
             price: "₹16000",
@@ -59,6 +67,7 @@ const get = (request , response) =>{
         {
             id: 9,
             name: "Redmi Note 11 Pro",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/717DeSsnXpL._SL1500_.jpg",
             price: "₹17000",
@@ -66,6 +75,7 @@ const get = (request , response) =>{
         {
             id: 10,
             name: "Redmi Note 11",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/81zLNgcvlaL._AC_SS450_.jpg",
             price: "₹23500",
@@ -73,6 +83,7 @@ const get = (request , response) =>{
         {
             id: 11,
             name: "Samsung Galaxy A13 ",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/81i1A1MgXBL._SL1500_.jpg",
             price: "₹14500",
@@ -80,6 +91,7 @@ const get = (request , response) =>{
         {
             id: 12,
             name: "Samsung Galaxy S20",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/812yohjGZ2L._SY550_.jpg",
             price: "₹20500",
@@ -87,6 +99,7 @@ const get = (request , response) =>{
         {
             id: 13,
             name: "Asus Zenfone 8",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://dlcdnwebimgs.asus.com/gain/c05b6491-6d2b-48f0-81f0-d268840208d6/",
             price: "₹15500",
@@ -94,6 +107,7 @@ const get = (request , response) =>{
         {
             id: 14,
             name: "Asus Zenfone 7",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://dlcdnwebimgs.asus.com/gain/aefee30c-d71b-46ce-9c28-5cf6daf80a03/",
             price: "₹15999",
@@ -101,6 +115,7 @@ const get = (request , response) =>{
         {
             id: 15,
             name: "Google Pixel 4 XL",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://m.media-amazon.com/images/I/71oTy+incwL._SL1500_.jpg",
             price: "₹16000",
@@ -108,6 +123,7 @@ const get = (request , response) =>{
         {
             id: 16,
             name:   "Google Pixel 5",
+            catagory: "Mobile",
             ratting: "https://t4.ftcdn.net/jpg/02/74/86/39/360_F_274863915_4JnfDXwTaTsjg5w5e7keElOipBswjAte.jpg",
             image: "https://www.notebookcheck.net/uploads/tx_nbc2/4_zu_3_Teaser_Google_Pixel_5.jpg",
             price: "₹17000"
